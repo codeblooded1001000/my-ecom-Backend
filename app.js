7b6cebd5d39a0a53e93b5de64b36f9d2bff95ae9
Prateek Jha <prtkjha01@gmail.com>
2023-02-28 14:27:53 +0530

    created global router

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require("mongoose");
-const router = require('./router.ts')
+const router = require('./router.js')
 require('dotenv').config();
 const app = express();
 app.use(express.json());
